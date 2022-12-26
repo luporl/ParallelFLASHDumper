e4e15b7cc79f33a9d284cbe71c97b8e934a1d842
luporl <leandro.lupori@gmail.com>
2022-12-26 15:54:45 -0300

    Add output test, fix pin assignments

--- ParallelFLASHDumper_rpi.c
+++ ParallelFLASHDumper_rpi.c
@@ -9,27 +9,31 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <wiringPi.h>
-
-#define ADDR_STROBE     25  /* latch - orange */
-#define ADDR_DATAOUT    29  /* green */
-#define ADDR_CLOCK      28  /* yellow */
 
 /* define according to the size of your target chip */
 #define MAX_ADDR        0xFFFFFF    /* 16 MB */
 
-/* INPUT DATA pins */
-#define IN_DQ00         9
-#define IN_DQ01         7
-#define IN_DQ02         1
-#define IN_DQ03         2
-#define IN_DQ04         3
-#define IN_DQ05         5
-#define IN_DQ06         6
-#define IN_DQ07         14
+/*
+ * I/O DATA pins
+ *
+ * On Pi 3 and 4 UART pins (15, 16) are used for bluetooth by default.
+ */
+#define DQ0         8
+#define DQ1         9
+#define DQ2         7
+#define DQ3         0
+#define DQ4         1
+#define DQ5         2
+#define DQ6         3
+#define DQ7         4
 
-#define OE              8
-#define RST             23
+#define OE          5
+#define WE          12
+
+#define ADDR_DATA   13
+#define ADDR_CLOCK  6
 
 
 /* LSB is shifted out first */
@@ -41,7 +45,7 @@ static void shiftAddrByte(unsigned char byte)
         digitalWrite(ADDR_CLOCK, 0);
         delayMicroseconds(2);
 
-        digitalWrite(ADDR_DATAOUT, byte & 1);
+        digitalWrite(ADDR_DATA, byte & 1);
 
         digitalWrite(ADDR_CLOCK, 1);
         delayMicroseconds(2);
@@ -53,78 +57,133 @@ static void sendAddr(unsigned addr)
 {
     int i;
 
-    digitalWrite(ADDR_STROBE, LOW);
-
     for (i = 0; i < 3; i++) {
         shiftAddrByte(addr & 0xFF);
         addr >>= 8;
     }
 
     /* LATCH/STROBE */
-    digitalWrite(ADDR_STROBE, HIGH);
     delayMicroseconds(2);
-    digitalWrite(ADDR_STROBE, LOW);
-    delayMicroseconds(2);
-}
-
-static void usage(void)
-{
-    printf("nordump <dump_file>\n");
 }
 
 static void setup(void)
 {
     /* Set pin modes */
-    pinMode(ADDR_STROBE, OUTPUT);
-    pinMode(ADDR_DATAOUT, OUTPUT);
+    pinMode(ADDR_DATA, OUTPUT);
     pinMode(ADDR_CLOCK, OUTPUT);
 
-    pinMode(IN_DQ00, INPUT);
-    pinMode(IN_DQ01, INPUT);
-    pinMode(IN_DQ02, INPUT);
-    pinMode(IN_DQ03, INPUT);
-    pinMode(IN_DQ04, INPUT);
-    pinMode(IN_DQ05, INPUT);
-    pinMode(IN_DQ06, INPUT);
-    pinMode(IN_DQ07, INPUT);
+    pinMode(DQ0, INPUT);
+    pinMode(DQ1, INPUT);
+    pinMode(DQ2, INPUT);
+    pinMode(DQ3, INPUT);
+    pinMode(DQ4, INPUT);
+    pinMode(DQ5, INPUT);
+    pinMode(DQ6, INPUT);
+    pinMode(DQ7, INPUT);
 
     pinMode(OE, OUTPUT);
-    pinMode(RST, OUTPUT);
 }
 
-static void reset(void)
+static void output_test(void)
 {
-    /* TODO reset shift registers */
+    int v;
 
-    /* Reset flash */
-    digitalWrite(RST, HIGH);
-    digitalWrite(OE, HIGH);
-    delay(1);
-    digitalWrite(RST, LOW);
-    delay(1);
-    digitalWrite(RST, HIGH);
-    digitalWrite(OE, LOW);
-    delay(1);
+    printf("ouput_test:\n");
+
+    pinMode(DQ0, OUTPUT);
+    pinMode(DQ1, OUTPUT);
+    pinMode(DQ2, OUTPUT);
+    pinMode(DQ3, OUTPUT);
+    pinMode(DQ4, OUTPUT);
+    pinMode(DQ5, OUTPUT);
+    pinMode(DQ6, OUTPUT);
+    pinMode(DQ7, OUTPUT);
+
+    pinMode(OE, OUTPUT);
+    pinMode(WE, OUTPUT);
+
+    pinMode(ADDR_DATA, OUTPUT);
+    pinMode(ADDR_CLOCK, OUTPUT);
+
+    /* repeat simple patterns */
+    for (v = 0; v < 16; v++) {
+        int a;
+        int b;
+
+        if (v < 8) {    /* all on/off */
+            a = b = v & 1;
+        } else {        /* bit on/bit off */
+            a = v & 1;
+            b = !a;
+        }
+
+        digitalWrite(DQ0, a);
+        digitalWrite(DQ1, b);
+        digitalWrite(DQ2, a);
+        digitalWrite(DQ3, b);
+
+        digitalWrite(DQ4, a);
+        digitalWrite(DQ5, b);
+        digitalWrite(DQ6, a);
+        digitalWrite(DQ7, b);
+
+        digitalWrite(OE, a);
+        digitalWrite(WE, b);
+        digitalWrite(ADDR_DATA, a);
+        digitalWrite(ADDR_CLOCK,b);
+        delay(500);
+    }
+
+    /* test all values for each 4 outputs group */
+    for (v = 0; v < 16; v++) {
+        digitalWrite(DQ0, v & 1);
+        digitalWrite(DQ1, v & 2);
+        digitalWrite(DQ2, v & 4);
+        digitalWrite(DQ3, v & 8);
+
+        digitalWrite(DQ4, v & 1);
+        digitalWrite(DQ5, v & 2);
+        digitalWrite(DQ6, v & 4);
+        digitalWrite(DQ7, v & 8);
+
+        digitalWrite(OE, v & 1);
+        digitalWrite(WE, v & 2);
+        digitalWrite(ADDR_DATA, v & 4);
+        digitalWrite(ADDR_CLOCK, v & 8);
+        delay(500);
+    }
+
+    digitalWrite(DQ0, 0);
+    digitalWrite(DQ1, 0);
+    digitalWrite(DQ2, 0);
+    digitalWrite(DQ3, 0);
+    digitalWrite(DQ4, 0);
+    digitalWrite(DQ5, 0);
+    digitalWrite(DQ6, 0);
+    digitalWrite(DQ7, 0);
+
+    digitalWrite(OE, 0);
+    digitalWrite(WE, 0);
+
+    digitalWrite(ADDR_DATA, 0);
+    digitalWrite(ADDR_CLOCK, 0);
+}
+
+static void usage(void)
+{
+    printf(
+        "nordump [flags] <dump_file>\n"
+        "flags:\n"
+        "\t-o\toutput test\n"
+    );
+    exit(1);
 }
 
 int main(int argc, char const *argv[]) {
-    const char *dump_file;
+    const char *arg, *dump_file;
     FILE *f;
     unsigned char input;
     unsigned addr;
-
-    if (argc != 2) {
-        usage();
-        return 1;
-    }
-
-    /* Open dump file */
-    dump_file = argv[1];
-    f = fopen(dump_file, "wb");
-    if (f == NULL) {
-        printf("failed to open %s\n", dump_file);
-        return 1;
-    }
 
     /* Init wiringPi */
     if (wiringPiSetup() == -1) {
@@ -132,22 +191,54 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
 
-    /* Setup pins, reset chips */
+    /* Parse args */
+    if (argc != 2)
+        usage();
+
+    arg = argv[1];
+    if (arg[0] == '-') {
+        if (arg[1] == 0 || arg[2] != 0)
+            usage();
+
+        switch (arg[1]) {
+        case 'o':
+            output_test();
+            break;
+
+        default:
+            usage();
+        }
+        return 0;
+    }
+
+    dump_file = arg;
+
+    /* TODO implement and test flash dump */
+    printf("TODO\n");
+    return 1;
+
+    /* Open dump file */
+    f = fopen(dump_file, "wb");
+    if (f == NULL) {
+        printf("failed to open %s\n", dump_file);
+        return 1;
+    }
+
+    /* Setup pins */
     setup();
-    reset();
 
     for (addr = 0; addr <= MAX_ADDR; addr++) {
         sendAddr(addr);
 
         /* Read data */
-        input  = digitalRead(IN_DQ00);
-        input |= digitalRead(IN_DQ01) << 1;
-        input |= digitalRead(IN_DQ02) << 2;
-        input |= digitalRead(IN_DQ03) << 3;
-        input |= digitalRead(IN_DQ04) << 4;
-        input |= digitalRead(IN_DQ05) << 5;
-        input |= digitalRead(IN_DQ06) << 6;
-        input |= digitalRead(IN_DQ07) << 7;
+        input  = digitalRead(DQ0);
+        input |= digitalRead(DQ1) << 1;
+        input |= digitalRead(DQ2) << 2;
+        input |= digitalRead(DQ3) << 3;
+        input |= digitalRead(DQ4) << 4;
+        input |= digitalRead(DQ5) << 5;
+        input |= digitalRead(DQ6) << 6;
+        input |= digitalRead(DQ7) << 7;
 
         /* Save data */
         if (fputc(input, f) == EOF) {
